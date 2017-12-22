633b3db5fff8ca10c61d7afffb650e646e129593
1l0 <rh.plus001@gmail.com>
2017-12-23 08:48:28 +0900

    cgo in gen.go

--- gen.go
+++ gen.go
@@ -1,3 +1,8 @@
 package godot
 
 //go:generate git pull -s subtree godot_headers master
+
+// #cgo CFLAGS: -Igodot_headers -std=c11
+// #cgo linux LDFLAGS: -Wl,-unresolved-symbols=ignore-all
+// #cgo darwin LDFLAGS: -Wl,-undefined,dynamic_lookup
+import "C"
