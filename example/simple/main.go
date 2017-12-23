a8d38a2fe385921c0807ee93cbaa63d465689117
1l0 <rh.plus001@gmail.com>
2017-12-23 09:19:35 +0900

    regen

---
+++ example/simple/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/1l0/godot"
+)
+
+var (
+	api             godot.Godot_gdnative_core_api_struct
+	nativescriptApi godot.Godot_gdnative_ext_nativescript_api_struct
+)
+
+func init() {
+	api = godot.NewGodot_gdnative_core_api_struct()
+	defer godot.DeleteGodot_gdnative_core_api_struct(api)
+
+}
+func main() {}
