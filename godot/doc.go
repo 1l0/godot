a9c2b152fc0d5ea372296a8d7630ed45f73451e6
1l0 <rh.plus001@gmail.com>
2017-12-24 07:13:03 +0900

    remove godot/

--- godot/doc.go
+++
@@ -1,7 +0,0 @@
-// WARNING: This file has automatically been generated on Sun, 24 Dec 2017 06:43:30 JST.
-// By https://git.io/c-for-go. DO NOT EDIT.
-
-/*
-Package godot provides Go bindings for Godot Engine
-*/
-package godot
