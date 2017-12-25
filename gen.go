70cecdaf7a05c7f1839aefd7505bfc9e5a3457c2
1l0 <rh.plus001@gmail.com>
2017-12-25 12:29:14 +0900

    regen

--- gen.go
+++ gen.go
@@ -1,6 +1,7 @@
 package godot
 
-//go:generate git pull -s subtree godot_headers master
+//go:generate git pull -s subtree godot-headers master
+//go:generate swig -go -cgo -intgosize 32 -Igodot_headers godot.swig_
 
 // #cgo CFLAGS: -Igodot_headers -std=c11
 // #cgo linux LDFLAGS: -Wl,-unresolved-symbols=ignore-all
